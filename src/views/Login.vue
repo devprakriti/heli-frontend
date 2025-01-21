172f0a52cac3aee5a8ecaa1ba50a10338716c91c
NazmaCyberlink <nazushakya7@gmail.com>
2025-01-21 15:49:06 +0545

    added eye icon

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -26,14 +26,20 @@
         <p v-if="usernameError" class="text-red-500 text-sm">username is required</p>
       </div>
 
-      <div class="mb-6">
+      <div class="mb-6 relative">
         <input
           v-model="password"
-          type="password"
+          :type="showPassword ? 'text' : 'password'"
           placeholder="Password"
-          class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+          class="  w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
           :class="{'border-red-500': passwordError}"
         />
+         <span
+            class="absolute right-4 top-2.5 cursor-pointer"
+            @click="togglePasswordVisibility"
+          >
+            <i :class="showPassword ? 'pi pi-eye-slash text-gray-400' : 'pi pi-eye text-gray-400'"></i>
+          </span>
         <p v-if="passwordError" class="text-red-500 text-sm">Password is required</p>
       </div>
 
@@ -59,7 +65,8 @@ export default {
       errorMessage: "",
       usernameError: false,
       passwordError: false,
-      loading: false
+      loading: false,
+      showPassword: false
     };
   },
   computed: {
@@ -72,6 +79,9 @@ export default {
       this.usernameError = !this.username;
       this.passwordError = !this.password;
       return !this.usernameError && !this.passwordError;
+    },
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
     },
     async login() {
       this.errorMessage = "";
@@ -92,7 +102,7 @@ export default {
           this.$router.push("/dashboard");
           setTimeout(() => {
             window.location.reload(); 
-      }, 100); // Small delay to ensure navigation happens first
+      }, 100); 
         } else {
           // Show error message if login failed
           this.loading = false;  // Stop loading
